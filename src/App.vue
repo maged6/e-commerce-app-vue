43c4e3fa78cdc34e47a4238724fd7d923a5c62e9
maged-abdalmashi-roshdy <magedsiha@gmail.com>
2024-01-13 17:12:37 +0200

    snackbar color

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
       location="left bottom"
       max-width="300"
       timeout="3000"
-    > this 
+      color="rgb(76, 175, 80)"
+    > 
       {{ itemTitle }} has been added to your cart successfuly!
       <template v-slot:actions>
         <v-icon @click="bar = flase">mdi-close</v-icon>
